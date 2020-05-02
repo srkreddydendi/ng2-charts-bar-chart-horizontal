abd33f658df16cfb9e95f15543a8a0bb18a8dea0
srkreddydendi <srkreddydendi@gmail.com>
2020-05-02 22:10:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng2-charts-bar-template-npudqg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng2-charts-bar-template-npudqg)
---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  public barChartOptions: ChartOptions = {
+    responsive: false,
+  };
+  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType: ChartType = 'horizontalBar';
+  public barChartLegend = true;
+  public barChartPlugins = [];
+
+  public barChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
