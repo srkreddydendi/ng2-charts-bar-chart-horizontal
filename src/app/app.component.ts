e54a86c9dd83c8a43295841885aaecded6b7ea06
srkreddydendi <srkreddydendi@gmail.com>
2020-05-02 22:15:52 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,137 @@ export class AppComponent  {
   public barChartType: ChartType = 'horizontalBar';
   public barChartLegend = true;
   public barChartPlugins = [];
+  calculateMaxValue(data) {
+    let chartOptions = {
+      scaleOverride: true,
+      scaleShowVerticalLines: false,
+      responsive: true,
+      barWidth: 20,
+      tooltips: {
+        enabled: false
+      },
+      hover: {
+        animationDuration: 0
+      },
+      plugins: {
+        datalabels: {
+          display: function (context) {
+            // console.log(context);
+            if ((context.datasetIndex === 0 && context.dataIndex === 0) || (context.datasetIndex === 0 && context.dataIndex === 1) || (context.datasetIndex === 2 && context.dataIndex === 2) || (context.datasetIndex === 3 && context.dataIndex === 3) || (context.datasetIndex === 4 && context.dataIndex === 4)) {
+              return true;
+            }
+            else {
+              return false;
+            }
+          },
+          backgroundColor: 'white',
+          borderColor: 'white',
+          borderRadius: 1,
+          borderWidth: 0,
+          anchor: 'end',
+          color: 'black',
+          align: 'end',
+          offset: function (context) {
+            if ((context.datasetIndex === 0 && context.dataIndex === 0)) {
+              if (context.dataset.data[0] === 0) {
+                return '10';
+              }
+              else {
+                return '-15';
+              }
+            }
+            else if ((context.datasetIndex === 0 && context.dataIndex === 1)) {
+              if (context.dataset.data[1] === 0) {
+
+                return '10';
+              }
+              else {
+                return '-15';
+              }
+            }
+            else if ((context.datasetIndex === 2 && context.dataIndex === 2)) {
+              if (context.dataset.data[2] === 0) {
+                return '10';
+              }
+              else {
+                return '-15';
+              }
+            }
+            else if ((context.datasetIndex === 3 && context.dataIndex === 3)) {
+              if (context.dataset.data[3] === 0) {
+                return '10';
+              }
+              else {
+                return '-15';
+              }
+            }
+            else if ((context.datasetIndex === 4 && context.dataIndex === 4)) {
+              if (context.dataset.data[4] === 0) {
+                return '10';
+              }
+              else {
+                return '-15';
+              }
+            }
+          },
+          font: {
+            weight: 'normal',
+            size: 20
+          },
+          formatter: Math.round
+        }
+      },
+      pointLabels: {
+        display: true
+      },
+      scales:
+      {
+        fontColor: '#666',
+        xAxes: [{
+            display: true,
+            barPercentage: 0.90,
+            stacked: true,
+            color: "transparent",
+            ticks: {
+              beginAtZero: true,
+              fontFamily: "'Roboto', 'Helvetica Neue', sans-serif",
+              fontColor: "transparent",
+            },
+            scaleLabel: {
+              display: false
+            },
+            gridLines: {
+              display: true,
+              offsetGridLines: true,
+              color: "white",
+            },
+          }],
+        yAxes: [{
+            barPercentage: 0.40,
+            display: true,
+            stacked: true,
+            gridLines: {
+              display: false,
+              offsetGridLines: false
+            },
+            ticks: {
+              fontFamily: "'Roboto', 'Helvetica Neue', sans-serif",
+              fontSize: 14,
+            },
+          }]
+      },
+    };
+    let dataObj =
+    {
+      "planDate": "2018-04-12T00:00:00Z",
+      "shiftId": "1",
+      "totInProgressAppts": "1",
+      "totPendingAppts": "2",
+      "totPlannedAppts": "5",
+      "totcompletedAppts": "2"
+    }
+    return chartOptions;
+  }
 
   public barChartData: ChartDataSets[] = [
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
